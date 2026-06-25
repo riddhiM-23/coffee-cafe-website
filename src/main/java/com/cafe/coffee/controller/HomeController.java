ca0a70e2e8a85b22baf8cfb6532eb7b4d5657fcc
Riddhi Mandape <riddhimandape@gmail.com>
2026-06-25 15:06:59 +0530

    Initial commit

---
+++ src/main/java/com/cafe/coffee/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.cafe.coffee.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/menu")
+    public String menu(Model model) {
+        return "menu";
+    }
+}
